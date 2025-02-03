d7aad58a88aa1f2050dc2741a02f4a01feea1354
HarshCh0uhan <harshcoding2004@gmail.com>
2025-02-03 16:07:13 +0530

    login

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
 
@@ -8,7 +9,7 @@ const NavBar = () => {
   return (
     <nav className="navbar bg-neutral px-4 md:px-8 lg:px-12 xl:px-16">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl md:text-2xl">DevHub</a>
+        <Link to="/" className="btn btn-ghost text-xl md:text-2xl">DevHub</Link>
       </div>
       {user && <div className="flex-none gap-2">
         <div className='form-control font-semibold' >Welcome, {user.firstName}</div>
@@ -25,10 +26,10 @@ const NavBar = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 w-52 p-2 shadow bg-base-100 rounded-box z-[1]">
               <li>
-                <a className="justify-between">
+                <Link to="/profile" className="justify-between">
                   Profile
                   <span className="badge">New</span>
-                </a>
+                </Link>
               </li>
               <li><a>Settings</a></li>
               <li><a>Logout</a></li>
