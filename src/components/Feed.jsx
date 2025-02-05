784cf36b3f1bff7db34b527c11e1230868b9878f
HarshCh0uhan <harshcoding2004@gmail.com>
2025-02-05 19:32:42 +0530

    fix bugs

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,21 +3,22 @@ import { BASE_URL } from '../utils/constants';
 import { addFeed } from '../utils/feedSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
-import UserCard from './UserCard';
+import UserCard from './UserCard.jsx';
 
 const Feed = () => {
   const dispatch = useDispatch();
   const feed = useSelector((store) => store.feed)
   
   const getFeed = async () => {
-    if(feed) return
+    if (feed && feed.length > 0) return;
+
     try {
       const res = await axios.get(BASE_URL + "/feed", {withCredentials: true});
 
       dispatch(addFeed(res?.data))
     } catch (err) {
-      console.log(err);
-    }
+      console.error("Error fetching feed:", err?.response?.data || err);
+    }    
   }
 
   useEffect(() => {
